293bb8d39a44f353f09a179b28a66823110913d8
sjmoon83 <chachaprint@naver.com>
2023-05-04 21:42:07 +0900

    webtoon 230504

---
+++ screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "어늘의 웹뚠",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
